df31b5dd7ad699d8fafc7be3513c1a9a468ffbd1
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2023-12-05 09:34:14 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+/**------------
+ * Add dependency
+ * express(npm i express)
+ * mongoose (npm i mongoose)
+ * nodemon (npm i nodemon -g)
+ * dotenv (.env / npm i dotenv) -> sendgrid
+ * body-parser (npm i body-parser)
+ * bcrypt (npm i bcrypt)
+ * jsonwebtoken (npm i jsonwebtoken)
+ ------------**/
+
